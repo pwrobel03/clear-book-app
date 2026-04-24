e88f4e9b0038c1a5ee9cec6f4164aa517ded8b91
Piotr Wróbel <wrobelpiotr24@gmail.com>
2026-04-24 13:39:35 +0200

    feat(schedule): add endpoint to fetch and delete own medical services

---
+++ api/src/main/java/com/clearbook/api/schedule/dto/CreateDoctorServiceRequest.java
@@ -0,0 +1,21 @@
+package com.clearbook.api.schedule.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CreateServiceRequest {
+    @NotBlank
+    private String name;
+
+    @Min(15)
+    private int durationMinutes;
+
+    @NotNull
+    @Min(0)
+    private BigDecimal price;
+}
